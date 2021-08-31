8d4d983d5453d6486d4a539ca589fda363471bc1
dalkumar500 <dalkumar500@gmail.com>
2021-08-31 19:02:40 -0400

    created controller

---
+++ GameFinderAgile/Controllers/GameController.cs
@@ -0,0 +1,17 @@
+﻿using GameFinderAgile.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web.Http;
+
+namespace GameFinderAgile.Controllers
+{
+    public class GameController : ApiController
+    {
+       
+
+    }
+}
