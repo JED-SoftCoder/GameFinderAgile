e766a151c8bad79d9f51dc21fa6b0a35c3204c89
dalkumar500 <dalkumar500@gmail.com>
2021-08-31 19:06:33 -0400

    Created post

--- GameFinderAgile/Controllers/GameController.cs
+++ GameFinderAgile/Controllers/GameController.cs
@@ -11,7 +11,30 @@ namespace GameFinderAgile.Controllers
 {
     public class GameController : ApiController
     {
-       
+        private readonly GameFinderDbContext _context = new GameFinderDbContext();
+
+        //POST(create)
+        //api/Game
+        [HttpPost]
+        public async Task<IHttpActionResult> CreateGame([FromBody] Game model)
+        {
+            if (model is null)
+            {
+                return BadRequest("your request body cannot be empty");
+            }
+            // If the model is valid
+            if (ModelState.IsValid)
+            {
+                //Store the model in the database
+                _context.Games.Add(model);
+                int changeCount = await _context.SaveChangesAsync();
+
+                return Ok("Your Game was created");
+            }
+            //The model is not valid,go ahead and reject it
+            return BadRequest(ModelState);
+
+        }
 
     }
 }
